ae1bd422c5067d8375778c8ee13422109c17bcd4
EddyChen1023 <eddy102394@gmail.com>
2023-12-03 10:02:02 +0800

    測試

---
+++ home - 複製.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles_of_home.css">
+    <title>Login Page</title>
+</head>
+<body>
+    <h1>Logistics Tracking System</h1>
+    <div class="login-buttons">
+        <button class="login-btn" onclick="location.href='/logistics_tracking_system/driver_login.html'">Driver Login</button>
+        <button class="login-btn" onclick="location.href='/logistics_tracking_system/buyer_login.html'">Buyer Login</button>
+        <button class="login-btn" onclick="location.href='/logistics_tracking_system/seller_login.html'">Seller Login</button>
+        <!-- <div style="background-color: pink;">
+            <?php
+                //include 'php/GetData.php' ?></GetData>
+            ?>
+        </div> -->
+    </div>
+</body>
+</html>
